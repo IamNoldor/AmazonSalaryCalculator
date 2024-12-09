f3635418313e0ee50569289ceb9130607309f964
IamNoldor <noldorvlad@gmail.com>
2024-12-09 01:47:24 +0100

    v. 0.4

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ let extraHoursShort = ["ro", "nso", "shw"]
 let activeHours = [];
 let currentPageStyle = true
 let currentLang = "eng"
+let currentTheme = "dark"
 
 function calculate(answer = 0) {
     let ro = 0
@@ -179,7 +180,14 @@ function add() {
             input.setAttribute("data-key", "7")
             input.classList.add("formBodyInput")
             input.classList.add("lng")
-
+            input.addEventListener("mouseenter", function (e) {
+                input.style.borderBottom = currentTheme === "dark" ? '2px solid red' : '2px solid white'
+            })
+            input.addEventListener("mouseleave", function (e) {
+                input.style.borderBottom = currentTheme === "dark" ? '2px solid #f6e9e9' : '2px solid rgb(57, 51, 51)'
+            })
+            input.style.borderBottom = currentTheme === "dark" ? '2px solid #f6e9e9' : '2px solid rgb(57, 51, 51)'
+            input.style.color = currentTheme === "dark" ? 'white' : 'black'
             div1.appendChild(label);
             div2.appendChild(input);
             div3.appendChild(span);
@@ -281,3 +289,68 @@ lngSelect.addEventListener("change", function (e) {
         }
     }
 })
+
+function changeTheme() {
+    currentTheme = currentTheme === 'dark' ? "light" : "dark";
+    let body = document.body
+    let addButton = document.getElementById("addButton")
+    let options = document.getElementsByTagName("option");
+    let allElements = document.getElementsByTagName("*");
+    let inputs = document.getElementsByClassName("formBodyInput");
+    let forms = document.getElementsByTagName('form')
+    let formHead = document.getElementsByClassName("formHead")
+    let buttons = document.getElementsByTagName("button");
+    let hr = document.getElementsByTagName("hr")[0];
+    let formBody = document.getElementsByClassName("formBody")[1];
+    let formBodyInputs = document.getElementsByClassName("formBodyInput");
+
+    changeStyle(buttons, "border", "2px solid red", '2px solid rgb(57, 51, 51)')
+    changeStyle(buttons, "bg", "#121212", 'rgb(105, 94, 94)')
+    changeStyle(formHead, 'borderBottom', '2px solid white', '2px solid rgb(57, 51, 51)')
+    changeStyle(inputs, 'borderBottom', '2px solid #f6e9e9', '2px solid rgb(57 51 51)')
+    changeStyle(allElements, 'color', 'white', 'black')
+    changeStyle(forms, "bg", '#121212', '#695e5e')
+    changeStyle(forms, 'border', '2px solid #121212', '2px solid rgb(57, 51, 51)')
+    changeStyle(options, 'bg', '#121212', '#695e5e')
+
+    hr.style.border = currentTheme === "dark" ? '1px solid black' : '1px solid #121212'
+    body.style.backgroundColor = currentTheme === "dark" ? 'black' : '#7d7d7d'
+    addButton.style.backgroundColor = currentTheme === "dark" ? '#121212' : '#695e5e'
+    formBody.style.borderBottom = currentTheme === "dark" ? '2px solid white' : '2px solid rgb(57, 51, 51)'
+
+    for(let el of buttons) {
+        el.addEventListener("mouseenter", function (e) {
+            el.style.color = currentTheme === "dark" ? 'red' : "white"
+            el.style.border = currentTheme === "dark" ? '2px solid white' : '2px solid black'
+        })
+        el.addEventListener("mouseleave", function (e) {
+            el.style.color = currentTheme === "dark" ? 'white' : 'black'
+            el.style.border = currentTheme === "dark" ? '2px solid red' : '2px solid rgb(57, 51, 51)'
+        })
+    }
+    for(let el of formBodyInputs) {
+        el.addEventListener("mouseenter", function (e) {
+            el.style.borderBottom = currentTheme === "dark" ? '2px solid red' : '2px solid white'
+        })
+        el.addEventListener("mouseleave", function (e) {
+            el.style.borderBottom = currentTheme === "dark" ? '2px solid #f6e9e9' : '2px solid rgb(57, 51, 51)'
+        })
+    }
+    function changeStyle(array, kind, firstColor, secondColor) {
+
+        for (let el of array) {
+            if (kind === 'bg') {
+                el.style.backgroundColor = currentTheme === "dark" ? firstColor : secondColor
+            }
+            if (kind === 'color') {
+                el.style.color = currentTheme === "dark" ? firstColor : secondColor
+            }
+            if (kind === 'border') {
+                el.style.border = currentTheme === "dark" ? firstColor : secondColor
+            }
+            if (kind === 'borderBottom') {
+                el.style.borderBottom = currentTheme === "dark" ? firstColor : secondColor
+            }
+        }
+    }
+}
